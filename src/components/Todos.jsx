39e2eecb66d005844f8fe44b82fcccd628de2d1c
Sujal Shrestha <sujal8696@gmail.com>
2025-06-15 21:55:54 +0545

    update by taking prompt

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { removeTodo } from "../features/todo/todoSlice";
+import { removeTodo, updateTodo } from "../features/todo/todoSlice";
 
 const Todos = () => {
   const todos = useSelector((state) => state.todos);
   const dispatch = useDispatch();
+
+  const handleUpdate = (id, text) => {
+    const newText = prompt("Update your todo:", text);
+    if (newText) {
+      dispatch(updateTodo({ id, text: newText }));
+    }
+  };
 
   return (
     <div>
       {todos.map((todo) => (
         <div key={todo.id}>
           <span>{todo.text}</span>
+          <button onClick={() => handleUpdate(todo.id, todo.text)}>
+            update
+          </button>
           <button onClick={() => dispatch(removeTodo(todo.id))}>Remove</button>
         </div>
       ))}
